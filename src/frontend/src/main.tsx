9aa73b5f5d8ccb728eb4e5fa24597232b4f521a0
552020 <stefanolombardo@posteo.de>
2025-07-30 11:44:21 +0200

    feat: scaffold new main for siwe integration

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,9 +1,48 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+
+// const rootElement = document.getElementById("root");
+// if (rootElement) {
+//   const root = ReactDOM.createRoot(rootElement);
+//   root.render(<App />);
+// }
+
 import "./index.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Actors from "./contexts/Actors.tsx";
+import App from "./App.tsx";
+import AuthGuard from "./contexts/AuthGuard.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { SiweIdentityProvider } from "ic-siwe-js/react";
+import { WagmiProvider } from "wagmi";
+import { wagmiConfig } from "./wagmi/wagmi.config.ts";
+import { canisterId } from "../../ic_siwe_provider/declarations/index";
+import { Toaster } from "@/components/ui/toaster.tsx";
+
+export const queryClient = new QueryClient();
+
 const rootElement = document.getElementById("root");
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<App />);
+
+if (!rootElement) {
+  throw new Error("Root element not found");
 }
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <SiweIdentityProvider canisterId={canisterId}>
+          <Actors>
+            <AuthGuard>
+              <App />
+              <Toaster />
+            </AuthGuard>
+          </Actors>
+        </SiweIdentityProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+);
